cf33be1f478514549ba72de82ef92119b85e5135
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2022-11-17 05:26:57 -0300

    feat: improve 2 player mode

--- space-invaders/game.py
+++ space-invaders/game.py
@@ -1,6 +1,7 @@
 import pygame, sys
 
 from space_invaders import SpaceInvaders
+from menu import Menu
 
 if __name__ == '__main__':
   pygame.init()
@@ -13,8 +14,9 @@ if __name__ == '__main__':
   pygame.display.set_icon(pygame.image.load('./graphics/red.png'))
 
   space_invaders = SpaceInvaders(screen_width, screen_height, players=2)
+  menu = Menu(screen_width, screen_height, screen)
 
-  mode = 'game'
+  mode = 'menu'
 
   ALIENLASER = pygame.USEREVENT + 1
   pygame.time.set_timer(ALIENLASER, 800)
@@ -28,8 +30,23 @@ if __name__ == '__main__':
         if event.type == ALIENLASER:
           space_invaders.alien_shoot()
 
-    screen.fill((30, 30, 30))
-    space_invaders.run(screen)
+      screen.fill((30, 30, 30))
+      space_invaders.run(screen)
 
-    pygame.display.flip()
-    clock.tick(60)
+      pygame.display.flip()
+      clock.tick(60)
+    
+    elif mode == 'menu':
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+          mode = 'menu'
+ 
+      screen.fill((30, 30, 30)) 
+      menu.run(screen)
+      pygame.display.flip()
+      clock.tick(60)
+
+   
